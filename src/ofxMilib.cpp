54ce2f45f50c779c080891249da0e86adf38b208
barneyElDinosaurio <marioalejandro1313@gmail.com>
2015-08-16 14:47:23 -0500

    first commit

---
+++ src/ofxMilib.cpp
@@ -0,0 +1,11 @@
+#include "ofxCircleParticles.h"
+
+
+
+void ofxCircleParticles::dibujarCirculo(int x, int y, int r){
+ofCircle(x,y,r);
+}
+
+void ofxCircleParticles::dibujar(){
+	
+}
---
+++ src/ofxMilib.h
@@ -0,0 +1,19 @@
+//
+//  ofxCircleParticles.cpp
+//
+//
+//  Created by marioAlzate on 1/10/14.
+//
+//
+
+#pragma once
+#include "ofMain.h"
+
+class ofxCircleParticles{
+
+
+public:
+	int X,Y,R;
+	void dibujarCirculo(int x, int y, int r);
+	void dibujar();
+};
